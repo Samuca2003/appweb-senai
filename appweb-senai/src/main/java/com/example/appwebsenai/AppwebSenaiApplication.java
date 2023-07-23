29445d54255dd46a46c3eb1c60109a2f925fbc8e
Samuca2003 <112135974+Samuca2003@users.noreply.github.com>
2023-07-23 17:30:35 -0300

    Add files via upload

---
+++ appweb-senai/src/main/java/com/example/appwebsenai/AppwebSenaiApplication.java
@@ -0,0 +1,12 @@
+package com.example.appwebsenai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppwebSenaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppwebSenaiApplication.class, args);
+    }
+}
