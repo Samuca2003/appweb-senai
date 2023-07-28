023d813536bd0023720e849065766bc44b1edbd3
Samuca2003 <112135974+Samuca2003@users.noreply.github.com>
2023-07-27 22:27:44 -0300

    Adicionando conexão ao MYSQL

--- appwebsenai/mvnw.cmd
+++ appwebsenai/mvnw.cmd
@@ -1,3 +1,5 @@
+#Exemplo de utilização GET, POST, DELETE, PUT
+
 @REM ----------------------------------------------------------------------------
 @REM Licensed to the Apache Software Foundation (ASF) under one
 @REM or more contributor license agreements.  See the NOTICE file
