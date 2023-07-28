83dc7be00705b4777556ab067511544ed680d392
Samuca2003 <112135974+Samuca2003@users.noreply.github.com>
2023-07-27 22:18:08 -0300

    #Adicionado entidade Conta

--- appwebsenai/src/main/java/com/example/controller/Controller.java
+++ appwebsenai/src/main/java/com/example/controller/Controller.java
@@ -1,5 +1,6 @@
-package com.example.appwebsenai.controller;
+#Adicionado entidade Conta
 
+package com.example.appwebsenai.controller;
 import com.example.appwebsenai.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
