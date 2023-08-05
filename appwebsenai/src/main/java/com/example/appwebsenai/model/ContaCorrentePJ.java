cc040d2602978a974f6a84b1d1238875b5aaad1e
Samuca2003 <112135974+Samuca2003@users.noreply.github.com>
2023-08-05 11:31:19 -0300

    Add files via upload

---
+++ appwebsenai/src/main/java/com/example/appwebsenai/model/ContaCorrentePJ.java
@@ -0,0 +1,5 @@
+package com.example.appwebsenai.model;
+
+public class ContaCorrentePJ extends Conta{
+
+}
---
+++ appwebsenai/src/main/java/com/example/appwebsenai/view/BancoView.java
@@ -0,0 +1,36 @@
+package com.example.appwebsenai.view;
+
+import com.example.appwebsenai.controller.BancoController;
+import com.example.appwebsenai.model.Conta;
+import com.example.appwebsenai.model.ContaCorrentePF;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
+
+@RestController
+public class BancoView {
+
+    @Autowired
+    private BancoController bancoController;
+    @GetMapping("/consultaSaldo")
+    public Double consultaSaldo(){
+        ContaCorrentePF conta = new ContaCorrentePF();
+        conta.setSaldo(100D);
+        return bancoController.consultaSaldo(conta);
+
+    }
+
+    @PostMapping("/criaconta")
+    public ContaCorrentePF criarConta(@PathParam("name") String name) throws Exception {
+        return bancoController.criarConta(name);
+    }
+
+    @GetMapping("/consultaconta")
+    public ContaCorrentePF consultaConta(@PathParam("name") String name){
+        return bancoController.consultaConta(name);
+    }
+
+}
