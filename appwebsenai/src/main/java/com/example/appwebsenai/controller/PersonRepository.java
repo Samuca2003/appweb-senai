dd45a7278f9bcb1681e4fcb03ea41062fc13c4e3
Samuca2003 <112135974+Samuca2003@users.noreply.github.com>
2023-08-05 14:46:18 -0300

    Delete appwebsenai directory

--- appwebsenai/src/main/java/com/example/appwebsenai/controller/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.appwebsenai.controller;
-
-import com.example.appwebsenai.model.Person;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PersonRepository extends CrudRepository<Person, Integer > {
-}
